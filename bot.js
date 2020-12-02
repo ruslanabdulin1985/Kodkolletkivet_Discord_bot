7a21b754385f5496dc85001292a5665151ae48a5
Ruslan Abdulin <ruslanabdulin1985@gmail.com>
2020-12-02 19:22:58 +0100

    added a simple http server to bind a port as HEROKU asks

--- bot.js
+++ bot.js
@@ -2,6 +2,14 @@ require('dotenv').config()
 
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const http = require('http')
+const port = 8080
+
+// start server just to bind a port
+http.createServer((req, res) => {
+    res.writeHead(200)
+    res.end('Hello world Ruslan\n')
+}).listen(port)
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
